46c8542b02ce2baeae21f4167ae64612878fccd3
Noah <noah@dreckbu.de>
2023-12-03 21:21:03 +0100

    03.2 ✅

--- 03/main.py
+++ 03/main.py
@@ -72,10 +72,10 @@ def main():
                   f' decimal_place={get_decimal_place(line, char_index)}'
                   f' has_right_neighbor_digit_with_adjacent_special_char={has_right_neighbor_digit_with_adjacent_special_char(lines, line_index, char_index)}')
 
-            if c.isnumeric() and (has_adjacent_special_char(lines, line_index, char_index)
+            if c.isnumeric() and (previous_char_relevant
+                                  or has_adjacent_special_char(lines, line_index, char_index)
                                   or has_right_neighbor_digit_with_adjacent_special_char(lines, line_index,
-                                                                                         char_index)
-                                  or previous_char_relevant):
+                                                                                         char_index)):
 
                 decimal_place = get_decimal_place(line, char_index)
                 sum_ += int(c) * (10 ** decimal_place)
@@ -86,5 +86,105 @@ def main():
     print(sum_)
 
 
+def has_left_neighboring_number(line: str, char_index: int) -> bool:
+    if char_index - 1 < 0:
+        return False
+
+    return line[char_index - 1].isnumeric()
+
+
+def count_adjacent_numbers(lines: list[str], line_index: int, char_index: int) -> tuple[int, list[tuple[int, int]]]:
+    line_limit = len(lines)
+    char_limit = len(lines[0])
+
+    adjacent_numbers = 0
+    positions = []
+
+    for offset in adjacent_char_offets:
+        char_offset = offset[1]
+        new_line_index = line_index + offset[0]
+        new_char_index = char_index + char_offset
+
+        if new_line_index < 0 \
+                or new_line_index >= line_limit \
+                or new_char_index < 0 \
+                or new_char_index >= char_limit:
+            continue
+
+        c = lines[new_line_index][new_char_index]
+
+        if c.isnumeric() and (char_offset == -1 or not has_left_neighboring_number(lines[new_line_index], new_char_index)):
+            adjacent_numbers += 1
+            positions.append((new_line_index, new_char_index))
+
+    return adjacent_numbers, positions
+
+
+def grow_numbers(lines: list[str], positions: list[tuple[int, int]]) -> list[int, int]:
+    numbers = []
+
+    for position in positions:
+        line_index, char_index = position
+
+        current_char_index = char_index
+        current_char = lines[line_index][current_char_index]
+
+        digits = [current_char]
+
+        current_char_index -= 1
+        current_char = lines[line_index][current_char_index]
+
+        while current_char_index >= 0 and current_char.isnumeric():
+            digits.insert(0, current_char)
+
+            current_char_index -= 1
+            current_char = lines[line_index][current_char_index]
+
+        current_char_index = char_index + 1
+        current_char = lines[line_index][current_char_index]
+
+        while current_char_index <= len(lines[0]) - 1 and current_char.isnumeric():
+            digits.append(current_char)
+
+            current_char_index += 1
+            try:
+                current_char = lines[line_index][current_char_index]
+            except IndexError:
+                print('stopping')
+
+        numbers.append(int(''.join(digits)))
+
+    return numbers
+
+
+def main2():
+    lines = read_input()
+
+    sum = 0
+
+    for line_index, line in enumerate(lines):
+        for char_index, c in enumerate(line):
+            if c == '*':
+                adjacent_numbers, positions = count_adjacent_numbers(lines, line_index, char_index)
+
+                if adjacent_numbers != 2:
+                    continue
+
+                num_1, num_2 = grow_numbers(lines, positions)
+
+                print(f'gear at line={line_index}'
+                      f' char={char_index}'
+                      f' c={c}'
+                      f' adjacent_numbers = {adjacent_numbers}'
+                      f' positions = {positions}'
+                      f' num_1 = {num_1}'
+                      f' num_2 = {num_2}')
+
+                sum += num_1 * num_2
+
+    print(sum)
+
+
 if __name__ == '__main__':
     main()
+    main2()
