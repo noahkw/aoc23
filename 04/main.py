21f0c2321712c1c3f8ad4c27cb29ce64765d5adb
Noah <noah@dreckbu.de>
2023-12-04 19:15:23 +0100

    04.2 ✅

--- 04/main.py
+++ 04/main.py
@@ -21,13 +21,20 @@ class ScratchCard:
     id: int
 
     def __str__(self):
-        return (f"Card {self.id}: {' '.join([str(n) for n in self.winning_numbers])} "
-                f"| {' '.join([str(n) for n in self.scratched_numbers])} --> Value: {self.value()}")
+        return (f"Card {self.id}: {' '.join([str(n) for n in self.winning_numbers])}"
+                f" | {' '.join([str(n) for n in self.scratched_numbers])} --> Value: {self.value()},"
+                f" Number winners: {self.number_winners()}")
+
+    def winners(self) -> set[int]:
+        return self.winning_numbers.intersection(self.scratched_numbers)
 
     def value(self) -> int:
-        winners = self.winning_numbers.intersection(self.scratched_numbers)
+        winners = self.winners()
 
         return 0 if len(winners) == 0 else 2 ** (len(winners) - 1)
+
+    def number_winners(self) -> int:
+        return len(self.winners())
 
     @staticmethod
     def parse(line: str) -> 'ScratchCard':
@@ -44,17 +51,43 @@ class ScratchCard:
         return scratch_card
 
 
+class Scratcher:
+    cards: list[ScratchCard]
+    card_amounts: dict[int, int] = dict()
+
+    def __init__(self, cards: list[ScratchCard]):
+        self.cards = cards
+
+    def run(self):
+        for card in self.cards:
+            self.card_amounts[card.id] = 1
+
+        for card in self.cards:
+            card_amount = self.card_amounts[card.id]
+
+            for i in range(card.number_winners()):
+                self.card_amounts[card.id + i + 1] += card_amount
+
+        total_cards = sum(self.card_amounts.values())
+        print(f"Amount of cards: {total_cards}")
+
+
 def main():
     lines = read_input()
 
     sum_of_points = 0
+    cards = []
 
     for line in lines:
         scratch_card = ScratchCard.parse(line)
+        cards.append(scratch_card)
         print(scratch_card)
         sum_of_points += scratch_card.value()
 
     print(f"Sum of points: {sum_of_points}")
+
+    scratcher = Scratcher(cards)
+    scratcher.run()
 
 
 if __name__ == '__main__':
